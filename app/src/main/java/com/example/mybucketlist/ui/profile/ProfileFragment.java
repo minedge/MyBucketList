f201df6e5f309e6fdd31debca64039a8632e7391
parkjooyeon <sb03063@naver.com>
2020-12-03 00:34:12 +0900

    phpprofile함수생성

--- app/src/main/java/com/example/mybucketlist/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mybucketlist/ui/profile/ProfileFragment.java
@@ -4,18 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.mybucketlist.MainActivity;
 import com.example.mybucketlist.R;
+import com.example.mybucketlist.login.PHPRequest;
+
+import java.net.MalformedURLException;
+
+import static com.example.mybucketlist.login.LoginActivity.url;
 
 public class ProfileFragment extends Fragment {
+
+    private TextView user_text, count_total, count_complete;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        user_text = root.findViewById(R.id.user_id);
+        count_total = root.findViewById(R.id.total);
+        count_complete = root.findViewById(R.id.complete);
+        Button pushBT = root.findViewById(R.id.push_button);
+        Button pullBT = root.findViewById(R.id.pull_button);
+
+        try {
+            PHPRequest request = new PHPRequest(url + "/profile.php");
+            request.PhPprofile_id(MainActivity._id);
+            user_text.setText(request.result_string);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
 
         return root;
     }
